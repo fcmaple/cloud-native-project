2b6c13e8bf116fd4e25d049f83b8cb297315455f
libos <benson60703@gmail.com>
2023-05-25 16:36:40 +0800

    add CORS

--- api/app/main.py
+++ api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .routes.user import router as user_router
 from .routes.passenger import router as passenger_router
@@ -25,6 +26,15 @@ tags_metadata = [
 ]
 
 app = FastAPI(openapi_tags=tags_metadata)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router)
 app.include_router(passenger_router)
